8b416bd02295474f1023c953036316f2a28060ec
xuejiao <lxj9207@163.com>
2016-08-03 18:18:52 +0800

    add filter

--- src/main/java/com/lxj/filter/AuthorityFilter.java
+++ src/main/java/com/lxj/filter/AuthorityFilter.java
@@ -1,7 +1,18 @@
 package com.lxj.filter;
 
+import com.alibaba.dubbo.rpc.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Created by lixuejiao on 16/8/3.
  */
-public class AuthorityFilter {
+public class AuthorityFilter implements Filter {
+    Logger logger = LoggerFactory.getLogger(AuthorityFilter.class);
+
+    public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
+        System.out.println("AuthorityFilter");
+        logger.info("AuthorityFilter");
+        return invoker.invoke(invocation);
+    }
 }
--- src/main/java/com/lxj/service/DemoServiceImpl.java
+++ src/main/java/com/lxj/service/DemoServiceImpl.java
@@ -5,7 +5,7 @@ import com.lxj.api.DemoService;
 /**
  * Created by lixuejiao on 16/7/24.
  */
-public class DemoServiceImpl implements DemoService {
+public class DemoServiceImpl implements DemoService{
     public String sayHello(String name) {
         return "hello , " + name;
     }
